7b418bcd216bee14fa93fe5995a504c5c836f9c1
imam-hossain1 <zunayedemon6t9@gmail.com>
2023-12-04 14:56:52 +0600

    login page modify

--- src/components/view/login/LoginPage.tsx
+++ src/components/view/login/LoginPage.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import { Button, Col,Row, message } from "antd";
+import { Button, Col, Row, message } from "antd";
 import Form from "@/components/Forms/Form";
 import { SubmitHandler } from "react-hook-form";
 import FormInput from "@/components/Forms/InputForm";
@@ -8,9 +8,6 @@ import { useRouter } from "next/navigation";
 import { useUserLogInMutation } from "@/redux/api/authApi";
 import { getUserInfo, storeUserInto } from "@/services/auth.service";
 import Link from "next/link";
-import { GithubFilled, GoogleCircleFilled } from "@ant-design/icons";
-
-
 
 type FormValues = {
   Email: string;
@@ -24,16 +21,16 @@ const LoginPage = () => {
   const onSubmit: SubmitHandler<FormValues> = async (data: any) => {
     try {
       const res = await userLogIn({ ...data }).unwrap();
-  
+
       if (res?.accessToken) {
         message.success("Login Successful");
         storeUserInto({ accessToken: res?.accessToken });
       }
-  
+
       const user = getUserInfo();
       //@ts-expect-error
-      const role:any = user.role;
-      
+      const role: any = user.role;
+
       if (user && role) {
         router.push(`/${role}/profile`);
       } else {
@@ -43,61 +40,77 @@ const LoginPage = () => {
       console.error(err.message);
     }
   };
-  
+
   return (
     <div className="bg-white shadow-2xl border-2 border-gray-200">
-      <Row
-        justify="center"
-        align="middle"
-        style={{
-          minHeight: "100vh",
-        }}
-      >
-        <Col sm={12} md={16} lg={10}>
-          <div>
-            <img
-              className="w-96 h-96"
-              src="https://i.ibb.co/gMndB2j/loginpage.png"
-              alt="login image"
-            />
-          </div>
-        </Col>
-        <Col sm={12} md={8} lg={8}>
-          <h3 className="text-center text-lg  py-1 rounded mx-auto mb-3 bg-gray-200">
-            First login your account
+      <div className="flex flex-col lg:flex-row justify-around">
+        <div>
+          <img
+            className="w-96 h-96"
+            src="https://i.ibb.co/gMndB2j/loginpage.png"
+            alt="login image"
+          />
+        </div>
+        <div
+          className="w-full lg:w-[30%] bg-white drop-shadow-2xl p-5 rounded-lg"
+          style={{
+            border: "1px solid #d9d9d9",
+            borderRadius: "5px",
+            padding: "15px",
+            marginBottom: "10px",
+          }}
+        >
+          <h3 className="text-center text-lg  py-1 rounded mx-auto mb-3 ">
+            Welcome Back
           </h3>
-          <div>
-            <Form submitHandler={onSubmit}>
-              <div>
+          <p className="text-center mb-2 mb-1">Please Login into your account</p>
+          <Form submitHandler={onSubmit}>
+            <div className="">
+              <div className="mb-3">
                 <FormInput
                   name="email"
                   type="text"
                   size="large"
-                  label="Email"
+                  placeholder="Email"
                 />
               </div>
-              <div
-                style={{
-                  margin: "15px 0px",
-                }}
-              >
+
+              <div className="mb-5">
                 <FormInput
                   name="password"
                   type="password"
                   size="large"
-                  label="User Password"
+                  placeholder="Strong Password"
                 />
               </div>
-              <Button type="primary" className="w-full" htmlType="submit">
-                Login
+            </div>
+            <div className="flex justify-center">
+              <Button
+                className=" bg-[#0C1A2D] w-full h-9"
+                type="primary"
+                htmlType="submit"
+              >
+                Log In
               </Button>
-            </Form>
-
-            <div className="flex justify-center items-center mt-4">
+            </div>
+            <div className="mt-5 flex justify-center">
+              <p className="text-[12px] font-semibold">
+                New to CarDev?{" "}
+                <Link href="/singup" className="text-red-400 underline">
+                  Register
+                </Link>
+              </p>
+            </div>
+          </Form>
+          {/* social login button */}
+{
+  /* <div className="flex justify-center items-center mt-4">
               <hr className="w-32 mr-2 text-[15px]" /> or Sing in with{" "}
               <hr className="ml-2 w-32" />
-            </div>
-            <div className="mt-5 flex justify-center ">
+            </div> */
+}
+{
+  /* <div className="mt-5 flex justify-center ">
               <Link
                 href="/facebook"
                 className="text-4xl rounded-full 
@@ -113,18 +126,10 @@ const LoginPage = () => {
               >
                 <GoogleCircleFilled />
               </Link>
-            </div>
-            <div className="mt-5 flex justify-center bg-gray-200">
-              <p className="text-[14px]">
-                New to CarDev?{" "}
-                <Link href="/singup" className="text-red-400 underline">
-                  Register
-                </Link>
-              </p>
-            </div>
-          </div>
-        </Col>
-      </Row>
+            </div> */
+}
+        </div>
+      </div>
     </div>
   );
 };
