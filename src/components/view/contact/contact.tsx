2596898ffe1fff26d40680b9a39339f163f41ee4
imam-hossain1 <zunayedemon6t9@gmail.com>
2024-01-31 23:08:05 +0600

    update contact form

--- src/components/view/contact/contact.tsx
+++ src/components/view/contact/contact.tsx
@@ -1,57 +1,51 @@
 "use client";
-import { SubmitHandler, useForm } from "react-hook-form";
+import Form from "@/components/Forms/Form";
+import BookingServiceForm from "../BookService/BookingServiceForm";
 
-interface FormValues {
-  name: string;
-  email: string;
-  phone: string;
-  subject: string;
-  message: string;
-}
 
 const Contact = () => {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm<FormValues>();
+  const onSubmit = (data: any) => {
 
-  const contactHandler: SubmitHandler<FormValues> = (data) => {
     const name = data.name;
     const email = data.email;
     const phone = data.phone;
-    const subject = data.subject;
-    const message = data.message;
+    const address = data.address;
+    const brand = data.brand;
+    const service_name = data.service_name;
+    const reservation_date = data.reservation_date;
+    const service_description = data.service_description;
 
     const number = "+971568703512";
-    const text =
+    const message =
       "Name: " +
       name +
-      "\n" +
-      "Email: " +
-      email +
       "\n" +
       "Phone: " +
       phone +
       "\n" +
-      "Subject: " +
-      subject +
+      "Email: " +
+      email +
       "\n" +
-      "Message: " +
-      message +
-      "\n";
+      "Address: " +
+      address +
+      "\n" +
+      "Brand: " +
+      brand +
+      "\n" +
+      "Service Name: " +
+      service_name +
+      "\n" +
+      "Reservation Date: " +
+      reservation_date +
+      "\n" +
+      "Service Description: " +
+      service_description;
 
-    const url = "https://wa.me/" + number + "?text=" + encodeURIComponent(text);
-    const newWindow = window.open(url, "_blank");
-    if (newWindow) {
-      newWindow.focus();
-    } else {
-      console.error("Unable to open a new window");
-    }
-
-    reset();
+    const url =
+      "https://wa.me/" + number + "?text=" + encodeURIComponent(message);
+    // window.open(url, "_blank").focus();;
   };
+
   return (
     <div id="contact" className="my-10">
       <style jsx>
@@ -89,78 +83,9 @@ const Contact = () => {
               Get In Touch
             </h1>
           </div>
-          <form
-            onSubmit={handleSubmit(contactHandler)}
-            className="lg:ml-4 whiteInput"
-          >
-            <input
-              type="text"
-              {...register("name", { required: "Name is required" })}
-              className="border border-gray-400 focus:outline-none w-full lg:w-96 p-2 mt-3"
-              placeholder="Enter Name"
-            />
-            {errors.name && (
-              <span className="text-red-500 text-sm">
-                {errors.name.message}
-              </span>
-            )}
-
-            <input
-              type="text"
-              {...register("email", { required: "Email is required" })}
-              className="border border-gray-400 focus:outline-none w-full lg:w-96 mt-3 p-2"
-              placeholder="Email"
-            />
-            {errors.email && (
-              <span className="text-red-500 text-sm">
-                {errors.email.message}
-              </span>
-            )}
-
-            <input
-              type="tel"
-              {...register("phone", { required: "Phone is required" })}
-              className="border border-gray-400 focus:outline-none w-full lg:w-96 mt-3 p-2"
-              placeholder="Phone"
-            />
-            {errors.phone && (
-              <span className="text-red-500 text-sm">
-                {errors.phone.message}
-              </span>
-            )}
-
-            <input
-              type="text"
-              {...register("subject", { required: "Subject is required" })}
-              className="border border-gray-400 focus:outline-none w-full lg:w-96 mt-3 p-2"
-              placeholder="Subject"
-            />
-            {errors.subject && (
-              <span className="text-red-500 text-sm">
-                {errors.subject.message}
-              </span>
-            )}
-
-            <textarea
-              {...register("message", { required: "Message is required" })}
-              className="mt-3 textarea textarea-bordered border border-gray-400 w-full lg:w-96 focus:outline-none h-28"
-              placeholder="Message"
-            />
-            {errors.message && (
-              <span className="text-red-500 text-sm">
-                {errors.message.message}
-              </span>
-            )}
-
-            <div className="flex justify-center">
-              <button
-                type="submit"
-                className="bg-primary border-0 py-3 rounded font-semibold text-white px-10 transition duration-500 w-48 mt-2"
-              >
-                Submit
-              </button>
-            </div>
-          </form>
+          <Form submitHandler={onSubmit}>
+            <BookingServiceForm />
+          </Form>
         </div>
       </div>
     </div>
--- src/components/view/service/ServicePage.tsx
+++ src/components/view/service/ServicePage.tsx
@@ -7,15 +7,16 @@ import { useAppSelector } from "@/redux/hooks";
 const ServicePage = () => {
   const { data: serviceDatas } = useAppSelector((state) => state.service);
   return (
-    <div>
-      <div className="mt-5">
+    <div className="mb-10">
+      <div className="w-80 -mt-2 mx-auto">
+        <h3 className="text-center bg-gray-300 p-1 rounded mb-5 -mt-5">
+          Quality Car Care Solutions
+        </h3>
+      </div>
+      <div className="my-5">
         <SearchFiltering />
       </div>
-      <div className="w-96 mx-auto  mt-7">
-        <h2 className="text-center bg-gray-300 p-1 rounded my-8">
-          Quality Car Care Solutions
-        </h2>
-      </div>
+
       {serviceDatas?.length < 1 && (
         <p className="text-3xl mb-20 font-semibold text-center text-red-400">
           No Data Found In Search Result
